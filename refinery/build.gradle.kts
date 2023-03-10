db226160da27cb279fd3e20279d5185fe1a72357
phonon <phonon9000@gmail.com>
2023-03-10 01:19:10 -0500

    update refineries version to match nodes version

--- refinery/build.gradle.kts
+++ refinery/build.gradle.kts
@@ -11,7 +11,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 version = ""
 
 // custom versioning flag
-val VERSION = "0.0.11"
+val VERSION = "0.0.13"
 
 // base of output jar name
 val OUTPUT_JAR_NAME = "nodes-refinery"
